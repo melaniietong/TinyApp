235d561e649b1218b96409c4d1f82c080173cf82
Melanie Tong <melaniietong@gmail.com>
2021-01-19 05:32:44 +0000

    Add a function to generate random 6 length string

--- express_server.js
+++ express_server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; 
 
+// Body Parser -- convert Buffer to string. Then add data to req() object.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// EJS -- site templates.
 app.set("view engine", "ejs");
 
 // Object stores all saved URLs with their short version.
@@ -26,6 +31,21 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console.
+  res.send("Ok");         // Respond with 'Ok' (we will replace this).
+});
+
+// Generates a random alphanumeric 6 character string.
+function generateRandomString() {
+  let output = '';
+  let alphanum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i <= 6; i++) {
+    output += alphanum[Math.floor(Math.random() * alphanum.length)]
+  }
+  return output;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
