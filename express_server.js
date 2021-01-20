743497e74aecf34292764a5a4c7050a75b358acb
Melanie Tong <melaniietong@gmail.com>
2021-01-20 04:46:52 +0000

    Add logout route functionality

--- express_server.js
+++ express_server.js
@@ -84,6 +84,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls"); 
 });
 
+// Logs out the user by clearing the cookie.
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls"); 
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
